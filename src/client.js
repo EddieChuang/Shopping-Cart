9762b34ee8f186e7f42a9b3fb612ff5fc61d704b
EddieChuang <az564123@gmail.com>
2018-03-09 22:36:34 +0800

    done

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
-import {Provodier, Provider} from 'react-redux';
+import {Provider} from 'react-redux';
 
 
 import {applyMiddleware, createStore} from 'redux';
